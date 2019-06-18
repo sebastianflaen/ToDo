3d1119d6a7f76c48c189b719f967163ca5de0613
SebastiansMac <sebastianflaen@gmail.com>
2019-06-18 21:35:41 +0200

    Initial Commit

---
+++ ToDo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDo
+//
+//  Created by Sebastian Sundet Flaen on 18/06/2019.
+//  Copyright © 2019 ssflaen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
